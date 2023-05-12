3d55667defed1ca0a7bb09313c6f675e078067c0
rubayatrifat <rubayatislam17@gmail.com>
2023-05-12 17:51:51 +0600

    Working...

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
-const useAbleFruit = document.querySelectorAll('.single-fruits.usable')
-const lockedFruit = document.querySelectorAll('.single-fruits.locked')
-const useAbleFruitAudio = document.getElementById('usable-fruit-click')
-const lokedFruitAudio = document.getElementById('loked-fruit-click')
-const balance = document.querySelector('')
+const useAbleFruit = document.querySelectorAll('.single-fruits.usable');
+const lockedFruit = document.querySelectorAll('.single-fruits.locked');
+const useAbleFruitAudio = document.getElementById('usable-fruit-click');
+const lokedFruitAudio = document.getElementById('loked-fruit-click');
+const BuyFruitAudio = document.getElementById('buy-fruit-click');
+const balance = document.querySelector('.main-money');
+const fruitsPrize = Array.from(document.querySelectorAll('#prize span'));
+const prices = fruitsPrize.map((price) => Number(price.textContent));
 
-useAbleFruit.forEach(sound => {
-    sound.addEventListener('click', () => {
-        useAbleFruitAudio.play();
-    });
+useAbleFruit.forEach((sound) => {
+  sound.addEventListener('click', () => {
+    useAbleFruitAudio.play();
+  });
 });
 
-lockedFruit.forEach(sound => {
-    sound.addEventListener('click', () => {
-        lokedFruitAudio.play();
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: "You haven't enough money to buy it!",
-          })
-    });
+lockedFruit.forEach((sound) => {
+  sound.addEventListener('click', () => {
+    isCanBuy();
+  });
 });
+
+function isCanBuy() {
+  const selectedFruitIndex = Array.from(lockedFruit).indexOf(event.currentTarget);
+  const selectedFruitPrice = Number(prices[selectedFruitIndex]);
+  if (Number(balance.textContent) < selectedFruitPrice) {
+    lokedFruitAudio.play();
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: "You haven't enough money to buy it!",
+    });
+  } else {
+    BuyFruitAudio.play()
+    Swal.fire({
+        icon: 'success',
+        title: 'Wow!',
+        text: "You buyed the food item Successfully",
+    });
+  }
+}
